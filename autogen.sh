74255d31c63cab35d555856f7441c98dd1585a69
Gergely POLONKAI (W00d5t0ck) <gergely@polonkai.eu>
2013-09-07 23:01:22 +0200

    Added libgd as a submodule, until GTK+ 3.10 comes out
    libgd provides some neat widgets, like a header similar to
    GTK+ 3.10's GtkHeaderBar, which comes in handy when generating the
    window headers.

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="astrognome"
+ACLOCAL_FLAGS="-I libgd $ACLOCAL_FLAGS"
 
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/src/astrognome.c) || {
