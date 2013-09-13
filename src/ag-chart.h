67dda14da027239109554a1e77e02a2608bdf4d2
Gergely Polonkai <gergely@polonkai.eu>
2013-09-14 01:25:46 +0200

    Started implementing load/save functionality

---
+++ src/ag-chart.h
@@ -0,0 +1,38 @@
+#ifndef __AG_CHART_H__
+#define __AG_CHART_H__
+
+#include <glib-object.h>
+#include <swe-glib.h>
+
+G_BEGIN_DECLS
+
+#define AG_TYPE_CHART         (ag_chart_get_type())
+#define AG_CHART(o)           (G_TYPE_CHECK_INSTANCE_CAST((o), AG_TYPE_CHART, AgChart))
+#define AG_CHART_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), AG_TYPE_CHART, AgChartClass))
+#define AG_IS_CHART(o)        (G_TYPE_CHECK_INSTANCE_TYPE((o), AG_TYPE_CHART))
+#define AG_IS_CHART_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE((k), AG_TYPE_CHART))
+#define AG_CHART_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS((o), AG_TYPE_CHART, AgChartClass))
+
+typedef struct _AgChart AgChart;
+typedef struct _AgChartClass AgChartClass;
+typedef struct _AgChartPrivate AgChartPrivate;
+
+struct _AgChart {
+    GsweMoment parent_instance;
+    AgChartPrivate *priv;
+};
+
+struct _AgChartClass {
+    GsweMomentClass parent_class;
+};
+
+GType ag_chart_get_type(void) G_GNUC_CONST;
+AgChart *ag_chart_new(void);
+
+void ag_chart_load_from_file(const gchar *path, GError **err);
+void ag_chart_save_to_file(const gchar *path, GError **err);
+
+G_END_DECLS
+
+#endif /* __AG_CHART_H__ */
+
