8313b4c2a6a59a45a998d416d2d36b1fe72c0b72
Gergely POLONKAI (W00d5t0ck) <gergely@polonkai.eu>
2013-09-21 21:54:35 +0200

    Added skeleton for the preferences code

---
+++ src/ag-preferences.h
@@ -0,0 +1,13 @@
+#ifndef __AG_PREFERENCES_H__
+#define __AG_PREFERENCES_H__
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+void ag_preferences_show_dialog(void);
+
+G_END_DECLS
+
+#endif /* __AG_PREFERENCES_H__ */
+
