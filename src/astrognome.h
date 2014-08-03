881b58080afb875272262e44a6ffd48852db3005
Gergely Polonkai <gergely@polonkai.eu>
2014-08-04 00:31:25 +0200

    Add ag_house_system_id_to_nick() and ag_house_system_nick_to_id()
    This is to convert between house system ID (required for chart generating)
    and GEnum nick (required for saving).

--- src/astrognome.h
+++ src/astrognome.h
@@ -1,6 +1,8 @@
 #ifndef __ASTROGNOME_H__
 #define __ASTROGNOME_H__
 
+#include <swe-glib.h>
+
 typedef struct {
     gboolean version;
     gboolean quit;
@@ -9,6 +11,9 @@ typedef struct {
 
 extern GtkFileFilter *filter_all;
 extern GtkFileFilter *filter_chart;
+
+const gchar *ag_house_system_id_to_nick(GsweHouseSystem house_system);
+GsweHouseSystem ag_house_system_nick_to_id(const gchar *nick);
 
 #endif /* __ASTROGNOME_H__ */
 
