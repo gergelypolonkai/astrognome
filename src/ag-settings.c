c38bba870750ea5beeed70070622b9400945cd91
Gergely Polonkai <gergely@polonkai.eu>
2014-09-04 23:37:00 +0200

    Add GSettings value for the default theme
    This obsoletes the Astrognome.state.chart schema, which is removed.

--- src/ag-settings.c
+++ src/ag-settings.c
@@ -2,15 +2,13 @@
 
 #define SETTINGS_SCHEMA_ID_MAIN   "eu.polonkai.gergely.Astrognome"
 #define SETTINGS_SCHEMA_ID_WINDOW "eu.polonkai.gergely.Astrognome.state.window"
-#define SETTINGS_SCHEMA_ID_CHART  "eu.polonkai.gergely.Astrognome.state.chart"
 
 static AgSettings *singleton = NULL;
 
-struct _AgSettingsPrivate {
+typedef struct _AgSettingsPrivate {
     GSettings *settings_main;
     GSettings *settings_window;
-    GSettings *settings_chart;
-};
+} AgSettingsPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE(AgSettings, ag_settings, G_TYPE_OBJECT);
 
@@ -21,7 +19,6 @@ ag_settings_init(AgSettings *settings)
 
     priv->settings_main   = g_settings_new(SETTINGS_SCHEMA_ID_MAIN);
     priv->settings_window = g_settings_new(SETTINGS_SCHEMA_ID_WINDOW);
-    priv->settings_chart  = g_settings_new(SETTINGS_SCHEMA_ID_CHART);
 }
 
 static void
@@ -32,7 +29,6 @@ ag_settings_dispose(GObject *object)
         );
 
     g_clear_object(&priv->settings_window);
-    g_clear_object(&priv->settings_chart);
     g_clear_object(&priv->settings_main);
 
     G_OBJECT_CLASS(ag_settings_parent_class)->dispose(object);
@@ -83,12 +79,4 @@ ag_settings_peek_window_settings(AgSettings *settings)
     AgSettingsPrivate *priv = ag_settings_get_instance_private(settings);
 
     return priv->settings_window;
-}
-
-GSettings *
-ag_settings_peek_chart_settings(AgSettings *settings)
-{
-    AgSettingsPrivate *priv = ag_settings_get_instance_private(settings);
-
-    return priv->settings_chart;
 }
